55c41cc557cd76cfc5a958bdfd51295e02facb59
Tejas <tejas@Tejass-MacBook-Air.local>
2025-12-24 11:06:53 +0530

    feat: sync dmall command and intents to mybot directory

--- mybot/src/index.js
+++ mybot/src/index.js
@@ -10,6 +10,7 @@ const client = new Client({
         GatewayIntentBits.MessageContent,
         GatewayIntentBits.GuildMembers,
         GatewayIntentBits.GuildMessageReactions,
+        GatewayIntentBits.GuildPresences,
     ]
 });
 
